4a4fde3ffb1b52a79cee4ab67cb8cc41d4f95183
Lane McIntosh <lanemcintosh@gmail.com>
2013-02-20 00:50:53 -0800

    Changed variance back to sigma in wiener.m

--- wiener.m
+++ wiener.m
@@ -1,13 +1,13 @@
-function [process] = wiener(startingValue, drift, trueVariance, N, fig)
-% INPUTS: startingValue, drift, variance, N, fig
+function [process] = wiener(startingValue, drift, sigma, N, T, fig)
+% INPUTS: startingValue, drift, variance, N, T, fig
 % OUTPUT: process, trueVariance
 % The Wiener process X_t = drift*t + sqrt(variance)*W_t has variance drift^2*t^2 - drift*t + variance^2*t
 
 %trueVariance = (drift^2)*(N^2) - (drift*N) + variance*N;
 % Note: inaccurate for small N
-sigma = sqrt((trueVariance - (drift^2)*(N^2) + (drift*N))/N);
+dt = T/N;
 
-displacements = drift + sigma*randn(N,1);
+displacements = drift + sigma*sqrt(dt)*randn(N,1);
 process = startingValue + cumsum(displacements);
 
 
