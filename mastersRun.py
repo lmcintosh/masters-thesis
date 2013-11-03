514d9726e91197543232fb46f26f808d323caf6e
Lane McIntosh <lanemcintosh@gmail.com>
2013-11-03 15:50:39 -0800

    added min max calculation for information

--- mastersRun.py
+++ mastersRun.py
@@ -12,7 +12,7 @@ from emailing import *
 
 
 try:
-    M     = 10e4
+    M     = 70
     nBins = 500
     adapt = linspace(0,25,100)
     
@@ -37,6 +37,12 @@ try:
         i_mem   = []
         i_pred  = []
         i_max   = []
+
+        minV = min(flatten(V))
+        maxV = max(flatten(V))
+        minC = min(flatten(stimulus))
+        maxC = max(flatten(stimulus))
+
         for i in xrange(shape(V)[1]-1):
             H, I = mutiN(V[:,i],stimulus[:,i],nBins,minV,maxV,minC,maxC)
             i_mem.append(I)
