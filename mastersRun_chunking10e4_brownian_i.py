e05103a01108d1b4300abef06c786f490d4549d4
Lane McIntosh <lanemcintosh@gmail.com>
2013-11-28 11:57:44 -0800

    finished adding binary calculation but need to test it

--- mastersRun_chunking10e4_brownian_i.py
+++ mastersRun_chunking10e4_brownian_i.py
@@ -25,15 +25,24 @@ try:
     elif version == 2:
         adapt = adapt[60:100]
     
-    steady_state_mem  = []
-    steady_state_pred = []
-    steady_state_max  = []
-    sum_mem           = []
-    sum_pred          = []
-    sum_max           = []
-    i_mem_fnA         = np.zeros((3,999))
-    i_pred_fnA        = np.zeros((3,999))
-    i_max_fnA         = np.zeros((3,999))
+    steady_state_mem   = []
+    steady_state_mem2  = []
+    steady_state_pred  = []
+    steady_state_pred2 = []
+    steady_state_max   = []
+    steady_state_max2  = []
+    sum_mem            = []
+    sum_mem2           = []
+    sum_pred           = []
+    sum_pred2          = []
+    sum_max            = []
+    sum_max2           = []
+    i_mem_fnA          = np.zeros((3,999))
+    i_mem_fnA2         = np.zeros((3,999))
+    i_pred_fnA         = np.zeros((3,999))
+    i_pred_fnA2        = np.zeros((3,999))
+    i_max_fnA          = np.zeros((3,999))
+    i_max_fnA2         = np.zeros((3,999))
     
     for a in adapt:
         # run simulation
@@ -75,23 +84,42 @@ try:
         steady_state_mem.append(i_mem[-1])
         steady_state_pred.append(i_pred[-1])
         steady_state_max.append(i_max[-1])
+        steady_state_mem2.append(i_mem2[-1])
+        steady_state_pred2.append(i_pred2[-1])
+        steady_state_max2.append(i_max2[-1])
         
         sum_mem.append(sum(i_mem))
         sum_pred.append(sum(i_pred))
         sum_max.append(sum(i_max))
+
+        sum_mem2.append(sum(i_mem2))
+        sum_pred2.append(sum(i_pred2))
+        sum_max2.append(sum(i_max2))
         
         if a < 0.1:
             i_mem_fnA[0,:]  = asarray(i_mem)
             i_pred_fnA[0,:] = asarray(i_pred)
             i_max_fnA[0,:]  = asarray(i_max)
+            
+            i_mem_fnA2[0,:]  = asarray(i_mem2)
+            i_pred_fnA2[0,:] = asarray(i_pred2)
+            i_max_fnA2[0,:]  = asarray(i_max2)
         elif abs(a - 6) < 0.1:
             i_mem_fnA[1,:]  = asarray(i_mem)
             i_pred_fnA[1,:] = asarray(i_pred)
             i_max_fnA[1,:]  = asarray(i_max)
+
+            i_mem_fnA2[1,:]  = asarray(i_mem2)
+            i_pred_fnA2[1,:] = asarray(i_pred2)
+            i_max_fnA2[1,:]  = asarray(i_max2)
         elif abs(a - 25) < 0.1:
             i_mem_fnA[2,:]  = asarray(i_mem)
             i_pred_fnA[2,:] = asarray(i_pred)
             i_max_fnA[2,:]  = asarray(i_max)
+
+            i_mem_fnA2[2,:]  = asarray(i_mem2)
+            i_pred_fnA2[2,:] = asarray(i_pred2)
+            i_max_fnA2[2,:]  = asarray(i_max2)
     
     
 
@@ -103,6 +131,15 @@ try:
 
         with open('masters_data_examples_' + str(stim_type) + '_a' + str(a) + '_' + str(datetime.date.today()) + '.pik', 'wb') as f:
             pickle.dump([i_mem_fnA, i_pred_fnA, i_max_fnA], f, -1)
+
+        with open('masters_data_steadyState2_' + str(stim_type) + '_a' + str(a) + '_' + str(datetime.date.today()) + '.pik', 'wb') as f:
+            pickle.dump([steady_state_mem2, steady_state_pred2, steady_state_max2], f, -1)
+
+        with open('masters_data_sum2_' + str(stim_type) + '_a' + str(a) + '_' + str(datetime.date.today()) + '.pik', 'wb') as f:
+            pickle.dump([sum_mem2, sum_pred2, sum_max2], f, -1)
+
+        with open('masters_data_examples2_' + str(stim_type) + '_a' + str(a) + '_' + str(datetime.date.today()) + '.pik', 'wb') as f:
+            pickle.dump([i_mem_fnA2, i_pred_fnA2, i_max_fnA2], f, -1)
 
 
 
