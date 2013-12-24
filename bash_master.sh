3536f1c6ce1feee477dbebc9bcd1f2778d699103
Lane McIntosh <lanemcintosh@gmail.com>
2013-12-23 22:40:20 -0800

    working on bash script to automate ssh'ing into corn and running simulations

--- bash_master.sh
+++ bash_master.sh
@@ -4,22 +4,30 @@
 echo "Please enter password for lanemc@corn.stanford.edu"
 read PASSWORD
 
+
+#cd ~/Git/masters-thesis/run_these/
+for i in 'ls ~/Git/masters-thesis/run_these/';
 # ssh into corn
-sshpass -p $PASSWORD ssh lanemc@corn.stanford.edu
+do sshpass -p $PASSWORD ssh lanemc@corn.stanford.edu 'cd ~/masters/python/; python "$i" &' &;
+
+done
+
+#sshpass -p $PASSWORD ssh lanemc@corn.stanford.edu 'cd ~/masters/python/; rm masters_data_examples2_* &'
+
 
 # prepare corn for running a long program in a screen session
-pagsh
-kinit;aklog 
-$PASSWORD
+#pagsh
+#kinit;aklog 
+#$PASSWORD
  
-screen -S masters
+#screen -S masters
 
-keeptoken
-source /tmp/.krbhold_lanemc.csh
+#keeptoken
+#source /tmp/.krbhold_lanemc.csh
 
 
-python mastersRun_chunking10e4_brownian_i.py
+#python mastersRun_chunking10e4_brownian_i.py
 
 
 # to detach screen execute keyboard shortcut
-xdotool key "ctrl+a+d" 
+#xdotool key "ctrl+a+d" 
