6f0df3a4e4e2371d78bd461f92c460612a9ee006
Lane McIntosh <lanemcintosh@gmail.com>
2013-12-27 23:16:16 -0800

    wrote python script to make host file for csshx

---
+++ makeHostFile.py
@@ -0,0 +1,18 @@
+#makeHostFile.py
+import numpy as np
+
+
+numCornServers = 30
+cornServers    = range(1,numCornServers+1)
+taus           = np.linspace(0.01,40,numCornServers)
+versions       = [1,2,3]
+
+
+hostFile = open('host_file.txt','a')
+for f in xrange(numCornServers):
+    if cornServers[f] < 10:
+        hostFile.write('lanemc@corn0' + str(cornServers[f]) + '.stanford.edu python ~/masters/python/mastersRun_chunking10e4_shotnoise.py ' + str(versions[0]) + ' ' + str(taus[f]) + '\n')
+    else:
+        hostFile.write('lanemc@corn' + str(cornServers[f]) + '.stanford.edu python ~/masters/python/mastersRun_chunking10e4_shotnoise.py ' + str(versions[0]) + ' ' + str(taus[f]) + '\n')
+
+hostFile.close()
