3c63b3d6df4cde16fb6134e5317fb494b827d086
Lane McIntosh <lanemcintosh@gmail.com>
2014-01-05 10:34:16 -0800

    fixed whitespace error

--- makeHostFile.py
+++ makeHostFile.py
@@ -11,8 +11,8 @@ versions       = [1,2,3]
 hostFile = open('host_file.txt','a')
 for f in xrange(numCornServers):
     if cornServers[f] < 10:
-        hostFile.write('lanemc@corn0' + str(cornServers[f]) + '.stanford.edu python ~/masters/python/mastersRun_chunking10e4_shotnoise.py ' + str(versions[0]) + ' ' + str(taus[f]) + str('</dev/null') + '>nohup.out 2>&1 &' + '\n')
+        hostFile.write('lanemc@corn0' + str(cornServers[f]) + '.stanford.edu python ~/masters/python/mastersRun_chunking10e4_shotnoise.py ' + str(versions[0]) + ' ' + str(taus[f]) + ' </dev/null >nohup.out 2>&1 &' + '\n')
     else:
-        hostFile.write('lanemc@corn' + str(cornServers[f]) + '.stanford.edu python ~/masters/python/mastersRun_chunking10e4_shotnoise.py ' + str(versions[0]) + ' ' + str(taus[f]) + str('</dev/null') + '>nohup.out 2>&1 &' + '\n')
+        hostFile.write('lanemc@corn' + str(cornServers[f]) + '.stanford.edu python ~/masters/python/mastersRun_chunking10e4_shotnoise.py ' + str(versions[0]) + ' ' + str(taus[f]) + ' </dev/null >nohup.out 2>&1 &' + '\n')
 
 hostFile.close()
